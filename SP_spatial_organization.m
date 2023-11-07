4e2f1a53a65304b3cdcc39aa5b7ff5adab3b9716
JacobCreamer <145397806+JacobCreamer@users.noreply.github.com>
2023-11-07 16:28:50 -0600

    Add AEP functionality

--- SP_spatial_organization.m
+++ SP_spatial_organization.m
@@ -1,7 +1,7 @@
 clc; clear; close all;
 
 warning('off','MATLAB:table:ModifiedAndSavedVarnames');
-addpath 'C:\USACE Work\Work Scripts\Spatial Analysis of Hazards - AEF and Profile\1st Application - LaRose - Golden Meadow\Shapefiles'
+addpath 'C:\USACE Work\Work Scripts\Spatial Analysis of Hazards - AEF and Profile\Code\Scituate Reach'
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% User Inputs %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -13,9 +13,8 @@ addpath 'C:\USACE Work\Work Scripts\Spatial Analysis of Hazards - AEF and Profil
 %     "CHS-LA_TS_SimBrfc2_Post_SP08005_Stat_AEF.csv", ...
 %     "CHS-LA_TS_SimBrfc2_Post_SP15444_Stat_AEF.csv"]; 
 
-% Option 2: find all h5 files in current folder
-in_file_struct = dir('*AEF.h5');
-in_file_list = {in_file_struct.name};
+% Option 2: find all h5 files in current folder (0 = no, 1 = yes)
+find_files = 1;
 
 
                       %%%% PROFILE DATA %%%%
@@ -23,21 +22,32 @@ in_file_list = {in_file_struct.name};
 % profile_file = table2array(readtable('system_4405000513_profile.csv'));
 
 % Option 2: load elevation information and shapefile (Specify filename)
-[A,R] = readgeoraster('LaRose_GM_NCEI_CUDEM.tif');
-[boundary_shp,shp_info] = shaperead('POLYLINE.shp');
+[A,R] = readgeoraster('Scituate_bathymetric_viewer.tif');
+[boundary_shp,shp_info] = shaperead('Scituate_Reach.shp');
+
 
 
                      %%%% Other Options %%%%
 % Units for profile data: 0 = meters, 1 = feet
-units = 1;
+units = 0;
 % specify if output csvs for swl & Hm0 are created: 0 = no, 1 = yes
-create_output_csvs = 0;
+create_output_csvs = 1;
 % Plot raster and shapefile: 0 = no, 1 = yes
 plot_raster = 0;
 % Downscale raster for viewing
 downscale_factor = 0.3;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if find_files == 1
+    in_file_struct = dir('*AEF.h5');
+    in_file_list = {in_file_struct.name};
+    if isempty(in_file_struct) == 1
+        in_file_struct = dir('*AEP.h5');
+        in_file_list = {in_file_struct.name};
+        use_aep = 1;
+    end
+end
 
 if exist('profile_file')
     shp_matrix = profile_file(:,1:2);
@@ -60,13 +70,23 @@ Hm0_out_table = table;
 swl_out_table = table;
 
 % read the first AEF file in list to pull AEF markers
-read_first_table = chs_h5_converter(string(in_file_list(1)));
-AEF_values = table(getfield(read_first_table,{1},'Table_StormData','AEF Values',{1:22}));
-AEF_values.Properties.VariableNames{1} = 'AEF Values';
+if use_aep == 1
+    read_first_table = chs_h5_converter(string(in_file_list(1)));
+    AEP_values = table(getfield(read_first_table,{1},'Table_StormData','AEP Values',{1:13}));
+    AEP_values.Properties.VariableNames{1} = 'AEP Values';
+else
+    read_first_table = chs_h5_converter(string(in_file_list(1)));
+    AEF_values = table(getfield(read_first_table,{1},'Table_StormData','AEF Values',{1:22}));
+    AEF_values.Properties.VariableNames{1} = 'AEF Values';
+end
 
 % append AEF markers to out tables
-Hm0_out_table = [Hm0_out_table AEF_values];
-swl_out_table = [swl_out_table AEF_values];
+if use_aep == 1
+    swl_out_table = [swl_out_table AEP_values];
+else
+    Hm0_out_table = [Hm0_out_table AEF_values];
+    swl_out_table = [swl_out_table AEF_values];
+end
 
 % create lists that will be needed
 sp_name_list = zeros(length(in_file_list),1);
@@ -89,15 +109,24 @@ for i = 1:length(in_file_list)
     lat(i) = sp_lat;
     lon(i) = sp_lon;
     
-    % get the AEF data for Hm0 and swl
-    Hm0_in_table = table(getfield(Data_out,{i},"Conv_Data",{1},'Table_StormData','Best Estimate AEF',{1:22}));
-    Hm0_in_table = renamevars(Hm0_in_table,"Var1",string(sp_name));
-    swl_in_table = table(getfield(Data_out,{i},"Conv_Data",{3},'Table_StormData','Best Estimate AEF',{1:22}));
-    swl_in_table = renamevars(swl_in_table,"Var1",string(sp_name));
+    % get the AEF/AEP data for Hm0 and swl
+    if use_aep == 1
+        swl_in_table = table(getfield(Data_out,{i},"Conv_Data",{1},'Table_StormData','Expected Value AEP',{1:13}));
+        swl_in_table = renamevars(swl_in_table,"Var1",string(sp_name));
+    else
+        Hm0_in_table = table(getfield(Data_out,{i},"Conv_Data",{1},'Table_StormData','Expected Value AEP',{1:22}));
+        Hm0_in_table = renamevars(Hm0_in_table,"Var1",string(sp_name));
+        swl_in_table = table(getfield(Data_out,{i},"Conv_Data",{3},'Table_StormData','Expected Value AEP',{1:22}));
+        swl_in_table = renamevars(swl_in_table,"Var1",string(sp_name));
+    end
 
     % append the AEF data to each out table
-    Hm0_out_table = [Hm0_out_table Hm0_in_table];
-    swl_out_table = [swl_out_table swl_in_table];
+    if use_aep == 1
+        swl_out_table = [swl_out_table swl_in_table];
+    else
+        Hm0_out_table = [Hm0_out_table Hm0_in_table];
+        swl_out_table = [swl_out_table swl_in_table];
+    end
 end
 
 % convert out tables to arrays
@@ -167,7 +196,7 @@ plot(shape_dist_ele_matrix(:,1),shape_dist_ele_matrix(:,2),'color','#36454F','Ha
 hold on
 datatip_sp_id = dataTipTextRow('SavePoint/Node ID',sorted_sp_name_list);
 
-% plot swl AEF at superimposed points along distance (along line of shapefile)
+% plot swl AEF/AEP at superimposed points along distance (along line of shapefile)
 for i = 1:height(swl_out_table)
     yvals = swl_data(i,2:width(swl_data));
     yvals_sorted = yvals(sort_order);
@@ -176,7 +205,11 @@ for i = 1:height(swl_out_table)
 end
 xlabel('Distance (m)')
 ylabel('Height (m)')
-title('Savepoints AEF - SWL')
+if use_aep == 0
+    title('Savepoints AEF - SWL')
+else
+    title('Savepoints AEP - SWL')
+end
 legend(string(swl_data(:,1)))
 grid
 hold off
@@ -184,45 +217,55 @@ hold off
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % plot profile of shapefile for Hm0 plot
-figure(3)
-
-plot(shape_dist_ele_matrix(:,1),shape_dist_ele_matrix(:,2),'color','#36454F','HandleVisibility','off','LineWidth',0.05,'LineStyle','-')
-hold on
-datatip_sp_id = dataTipTextRow('SavePoint/Node ID',sorted_sp_name_list);
-
-% plot Hm0 AEF at superimposed points along distance (along line of shapefile)
-for i = 1:height(Hm0_out_table)
-    yvals = Hm0_data(i,2:width(Hm0_data));
-    yvals_sorted = yvals(sort_order);
-    Hm0_plot = plot(xvals,yvals_sorted,'linestyle','-','Marker','.');
-    Hm0_plot.DataTipTemplate.DataTipRows(3) = datatip_sp_id;
+if use_aep == 0
+    figure(3)
+    
+    plot(shape_dist_ele_matrix(:,1),shape_dist_ele_matrix(:,2),'color','#36454F','HandleVisibility','off','LineWidth',0.05,'LineStyle','-')
+    hold on
+    datatip_sp_id = dataTipTextRow('SavePoint/Node ID',sorted_sp_name_list);
+    
+    % plot Hm0 AEF at superimposed points along distance (along line of shapefile)
+    for i = 1:height(Hm0_out_table)
+        yvals = Hm0_data(i,2:width(Hm0_data));
+        yvals_sorted = yvals(sort_order);
+        Hm0_plot = plot(xvals,yvals_sorted,'linestyle','-','Marker','.');
+        Hm0_plot.DataTipTemplate.DataTipRows(3) = datatip_sp_id;
+    end
+    xlabel('Distance (m)')
+    ylabel('Height (m)')
+    title('Savepoints AEF - Hm0')
+    legend(string(Hm0_data(:,1)))
+    grid
+    hold off
 end
-xlabel('Distance (m)')
-ylabel('Height (m)')
-title('Savepoints AEF - Hm0')
-legend(string(Hm0_data(:,1)))
-grid
-hold off
-
 %%%%%%%%%%%%%%%%%% Plot SWL and Hm0 Data from Table %%%%%%%%%%%%%%%%%%%%%%
 
 % create new sorted output tables based on distance along shapefile
-sorted_swl_out_table = table();
-sorted_swl_out_table = [sorted_swl_out_table AEF_values];
-sorted_Hm0_out_table = table();
-sorted_Hm0_out_table = [sorted_Hm0_out_table AEF_values];
+if use_aep == 0
+    sorted_swl_out_table = table();
+    sorted_swl_out_table = [sorted_swl_out_table AEF_values];
+    sorted_Hm0_out_table = table();
+    sorted_Hm0_out_table = [sorted_Hm0_out_table AEF_values];
+else
+    sorted_swl_out_table = table();
+    sorted_swl_out_table = [sorted_swl_out_table AEP_values];
+end
 
 for i = 1:length(sorted_sp_name_list)
     sorted_sp_name = sorted_sp_name_list(i);
     sorted_swl_out_table = [sorted_swl_out_table swl_out_table(:,string(sorted_sp_name))];
-    sorted_Hm0_out_table = [sorted_Hm0_out_table Hm0_out_table(:,string(sorted_sp_name))];
+    if use_aep == 0
+        sorted_Hm0_out_table = [sorted_Hm0_out_table Hm0_out_table(:,string(sorted_sp_name))];
+    end
 end
 
 % convert sorted out tables to arrays for plotting
 sorted_swl_data = table2array(sorted_swl_out_table);
-sorted_Hm0_data = table2array(sorted_Hm0_out_table);
+if use_aep == 0
+    sorted_Hm0_data = table2array(sorted_Hm0_out_table);
+end
 
-% plot savepoint swl AEF information from sorted table
+% plot savepoint swl AEF/AEP information from sorted table
 figure(4)
 for i = 1:height(sorted_swl_out_table)
     plot(sorted_swl_data(i,2:width(sorted_swl_data)),'linestyle','-','Marker','.');
@@ -230,27 +273,33 @@ for i = 1:height(sorted_swl_out_table)
 end
 xticks(1:1:width(swl_data));
 xticklabels(sorted_swl_out_table.Properties.VariableNames(2:end));
-title('Savepoints AEF - SWL')
+if use_aep == 0
+    title('Savepoints AEF - SWL')
+else
+    title('Savepoints AEP - SWL')
+end
 xlabel('Save Points')
 ylabel('Height (m)')
 legend(string(swl_data(:,1)))
 grid
 
 % plot savepoint Hm0 AEF information from sorted table
-figure(5)
-for i = 1:height(sorted_Hm0_out_table)
-    plot(sorted_Hm0_data(i,2:width(sorted_Hm0_data)),'linestyle','-','Marker','.');
-    hold on
+if use_aep == 0
+    figure(5)
+    for i = 1:height(sorted_Hm0_out_table)
+        plot(sorted_Hm0_data(i,2:width(sorted_Hm0_data)),'linestyle','-','Marker','.');
+        hold on
+    end
+    xticks(1:1:width(Hm0_data));
+    xticklabels(sorted_Hm0_out_table.Properties.VariableNames(2:end));
+    title('Savepoints AEF - Hm0')
+    xlabel('Save Points')
+    ylabel('Height (m)')
+    legend(string(Hm0_data(:,1)))
+    grid
+    
+    hold off
 end
-xticks(1:1:width(Hm0_data));
-xticklabels(sorted_Hm0_out_table.Properties.VariableNames(2:end));
-title('Savepoints AEF - Hm0')
-xlabel('Save Points')
-ylabel('Height (m)')
-legend(string(Hm0_data(:,1)))
-grid
-
-hold off
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% Crest Statistics %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -292,7 +341,7 @@ end
 
 figure(6)
 
-% plot swl aef data
+% plot swl AEF/AEP data
 
 for i = 1:height(swl_out_table)
     yvals = swl_data(i,2:width(swl_data));
@@ -301,6 +350,7 @@ for i = 1:height(swl_out_table)
     swl_plot.DataTipTemplate.DataTipRows(3) = datatip_sp_id;
     hold on
 end
+title('SWL & Crest Statistics')
 xlabel('Distance (m)')
 ylabel('Height (m)')
 legend(string(swl_data(:,1)))
@@ -356,6 +406,8 @@ end
 
 % create new CSVs for swl and Hm0 using the created out tables
 if create_output_csvs == 1
-    writetable(sorted_Hm0_out_table,'Hm0_out.csv');
+    if use_aep == 0
+        writetable(sorted_Hm0_out_table,'Hm0_out.csv');
+    end
     writetable(sorted_swl_out_table,'swl_out.csv');
 end
